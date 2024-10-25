044b631946c23bea039d0e5dcb4df7753296514d
hdXian <jason123325@gmail.com>
2024-10-25 23:23:54 +0900

    #3 1 - add business method on entity Item

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,5 +27,20 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items") // Category 클래스의 items 필드에 의해 매핑
     List<Category> categories = new ArrayList<>(); // 이 상품이 어느 카테고리에 속하는지
+
+    // add business logic
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int tmp = this.stockQuantity - quantity;
+
+        if(tmp < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = tmp;
+    }
 
 }
